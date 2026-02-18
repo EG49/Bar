0c86d29065276ba252a610b6ed3df9239a65c04d
Evans <egaibor812@gmail.com>
2026-02-17 21:44:31 -0500

    Agregando login funcional con la db

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for, session 
+from  config import get_connection
+from functools import wraps
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "user_id" not in session:
+            return redirect(url_for("show_login"))
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 app = Flask(__name__)
+print(app.url_map)
+app.secret_key = "super_secret_key"
+
 @app.context_processor
 def inject_user():
     user = {"nombre": "Evans"}  # solo para prueba
     return dict(user=user)
 @app.route("/inicio")
+@login_required
 def inicio():
     return render_template("inicio.html")
+
 
 @app.route("/vender")
 def vender():
@@ -37,9 +53,62 @@ def cobrar():
 def pedidos():
     return render_template("pedidos.html")
 
-@app.route("/")
-def login():
+@app.route("/", methods=["GET"])
+def show_login():
     return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        query = """
+            SELECT id, username, rol, debe_cambiar_password, otp_activo
+            FROM usuarios
+            WHERE username = %s
+              AND estado = TRUE
+              AND password_hash = crypt(%s, password_hash);
+        """
+
+        cur.execute(query, (username, password))
+        user = cur.fetchone()
+
+        if user:
+            session["user_id"] = user[0]
+            session["username"] = user[1]
+            session["rol"] = user[2]
+
+            cur.execute(
+                "UPDATE usuarios SET ultimo_login = NOW() WHERE id = %s;",
+                (user[0],)
+            )
+            conn.commit()
+
+            cur.close()
+            conn.close()
+
+            return redirect("/inicio")
+
+
+
+        cur.close()
+        conn.close()
+
+        return "Credenciales inválidas", 401
+
+    except Exception as e:
+        return str(e), 500
+
+#a
+# -----------------------
+# 3️⃣ Dashboard
+# -----------------------
+
+ 
 
 if __name__ == "__main__":
     app.run(debug=True)
